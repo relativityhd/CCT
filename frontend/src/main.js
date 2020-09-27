e97e98d04291513ee8f6c1d8b599c8aaba8c4c77
Ole Ofterdinger <wi19303@lehre.dhbw-stuttgart.de>
2020-09-27 16:08:00 +0200

    name

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import 'carbon-components/css/carbon-components.css';
+import CarbonComponentsVue from '@carbon/vue/src/index';
 
+Vue.use(CarbonComponentsVue);
 Vue.config.productionTip = false;
 
 new Vue({
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <cv-button @click="onClick">Hello {{yourName}}</cv-button>
   </div>
 </template>
 

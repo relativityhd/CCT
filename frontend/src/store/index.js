ec04c66221ca994685933fb1f3217cf6ef937eb4
Tobias Hölzer <tobiashoelzer@hotmail.com>
2020-09-29 14:08:52 +0200

    Add Location and Lang to Vuex Store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  // location 'rw' === Other Country
+  /**
+   * locations: de, ch, at, fr, it, gb, rw
+   * languages: de, en, fr, it
+   */
+  state: {
+    lang: "en",
+    location: "gb"
+  },
+  mutations: {
+    setLang(state, lang) {
+      state.lang = lang;
+    },
+    setLocation(state, location) {
+      state.location = location;
+    }
+  },
   actions: {},
   modules: {}
 });

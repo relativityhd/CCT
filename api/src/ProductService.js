bc7f94a8fad8e54839cc18ec91415986c1a673de
Ole Ofterdinger <wi19303@lehre.dhbw-stuttgart.de>
2020-09-30 18:53:55 +0200

    product service

--- api/src/ProductService.js
+++ api/src/ProductService.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const products = require('./products');
+
 
 router.get('/', function (req, res) {
-  res.send('Products-Service!')
+  res.send('Products-Service!');
 })
 
+router.get('/bodies', (req,res)=>{
+  res.json(products.bodies);
+})
+
+router.get('/doors',(req,res)=>{
+  res.json(products.doors);
+})
+
+router.get('/boards',(res,req)=>{
+  //req.json(products.boards);
+  req.send("he")
+})
 module.exports = router
---
+++ api/src/products.js
@@ -0,0 +1,38 @@
+exports.bodies = [
+    {
+      name: "body1",
+      price: 100
+    },
+    {
+      name: "body2",
+      price: 150
+    },  {
+      name: "body1",
+      price: 200
+    },
+  ]
+  
+ exports.boards= [{
+      name: "board",
+      price: 10
+    },
+    {
+      name: "body2",
+      price: 7
+    },  {
+      name: "body3",
+      price: 10
+  }]
+  
+exports.doors =   [{
+    name: "Gondoor",
+    price: 40
+  },
+  {
+    name: "Mordoor",
+    price: 50
+  },  {
+    name: "Hodor",
+    price: 10
+  }]
+  

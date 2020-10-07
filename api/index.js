7d2f0e9ee418254e44660312ca04af3c4ac3df4c
Ole Ofterdinger <wi19303@lehre.dhbw-stuttgart.de>
2020-10-07 12:33:24 +0200

    broken

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 3001
 
+app.use(bodyParser.json())
+app.use(cors())
 app.use('/billing-service', require('./src/BillingService'))
 app.use('/customer-service', require('./src/CustomerService'))
 app.use('/order-service', require('./src/OrderService'))
--- api/src/BillingService.js
+++ api/src/BillingService.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const router = express.Router()
-
+const products = require('./products')
+const { route } = require('./ProductService')
+const bodyParser = require('body-parser')
+const allProducts = [products.bodies,products.doors, products.boards].flat()
+var jsonParser = bodyParser.json()
 router.get('/', function (req, res) {
-  res.send('Billing-Service!')
+  res.send('Billing-Service!');
 })
+router.use(bodyParser.json())
 
+router.post('/bill',jsonParser, (req, res)=>{
+  console.log(req.body)
+  const receivedOrderList = req.body
+
+  const orderList = Array.from(receivedOrderList,(sentobject)=>{
+    return product = products.find((element)=>{
+      element.name == val.name
+    })
+  })
+
+  const total = orderList.reduce((acc,val)=>{
+    acc + price
+  })
+
+  res.json({
+    order: orderList,
+    total: total
+  })
+})
 module.exports = router

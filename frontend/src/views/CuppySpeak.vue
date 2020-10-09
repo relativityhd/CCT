e1302c9a77a03dfea2cb534c5f533c77c34a2235
Ole Ofterdinger <wi19303@lehre.dhbw-stuttgart.de>
2020-10-09 22:06:02 +0200

    cuppy images and speechbubble

---
+++ frontend/src/views/CuppySpeak.vue
@@ -0,0 +1,33 @@
+<template>
+<div id=cuppyspeako>
+  <button>Hilfe</button>
+  <button>NAMENAMENAME</button>
+  <button>Tobo ist doof</button>
+  <button>Philipp ist doof</button>
+  <button>Christopher ist doof</button>
+</div>
+</template>
+
+<script>
+
+export default {
+    
+}
+
+</script>
+
+<style lang="sass" scoped>
+#cuppyspeako
+  background-image: url(./../assets/bubble.svg)
+  background-repeat: no-repeat
+  background-size: contain
+  display: flex
+  flex-direction: column
+  height: max-content
+  width: max-content
+button
+  border: none
+  width: max-content
+  
+
+</style>

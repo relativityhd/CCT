d576c3966562d3acbdf1f847bd1f15209bcc38ed
Tobias Hölzer <tobiashoelzer@hotmail.com>
2020-10-11 19:05:53 +0200

    Style CategoriesOverview

--- frontend/src/views/CategoriesOverview.vue
+++ frontend/src/views/CategoriesOverview.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="overview-wrapper">
-    <div class="categories-container" v-for="ct in categories" :key="ct.id">
+    <h1>{{ $t('title') }}</h1>
+    <div class="categories-container">
       <cv-tile 
+        v-for="ct in categories"
+        :key="ct.id"
         kind="clickable"
         :to="ct.to"
+        class="category-tile"
         >
-        <h1>{{ct.name}}</h1><img :src="ct.imageUrl">
+        <img class="tile-image" :src="ct.imageUrl" alt="Image of Category">
+        <div class="tile-body">
+          <h3>{{ct.name}}</h3>
+        </div>
       </cv-tile>
     </div>
   </div>
@@ -23,9 +30,41 @@ export default {
   mounted() {
       Vue.axios.get('/catalogue/categories').then(res => {
         console.log(res)
-        this.categories = res.data.map(c => ({...c, to:`/categories/${c.id}`}))
+        this.categories = res.data.map(c => ({...c, to:`/category/${c.id}`}))
       })
   }
 }
 </script>
 
+<style lang="scss" scoped>
+.overview-wrapper{
+  width: 98%;
+  max-width: 1400px;
+  margin: 0 auto;
+}
+
+.categories-container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.category-tile{
+  text-align: left;
+  margin: 20px;
+}
+
+.tile-image{
+  width: 300px;
+}
+</style>
+
+<i18n>
+{
+  "en": {
+    "title": "View our Catalogue"
+  }
+}
+</i18n>

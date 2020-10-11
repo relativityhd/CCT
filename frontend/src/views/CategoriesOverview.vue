7f04ac0afcd455e74d9937698d1e3fda970db905
Amon Schneider <wi19102@dhbw-stuttgart.de>
2020-10-11 18:38:27 +0200

    Categories containers hinzugefügt

--- frontend/src/views/CategoriesOverview.vue
+++ frontend/src/views/CategoriesOverview.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="overview-wrapper">
-    Hello
+    <div class="categories-container" v-for="ct in categories" :key="ct.id">
+      <cv-tile 
+        kind="clickable"
+        :to="ct.to"
+        >
+        <h1>{{ct.name}}</h1><img :src="ct.imageUrl">
+      </cv-tile>
+    </div>
   </div>
 </template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  data() {
+    return {
+      categories: []
+    }
+  },
+  mounted() {
+      Vue.axios.get('/catalogue/categories').then(res => {
+        console.log(res)
+        this.categories = res.data.map(c => ({...c, to:`/categories/${c.id}`}))
+      })
+  }
+}
+</script>
+

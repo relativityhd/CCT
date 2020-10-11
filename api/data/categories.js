22972fac3335fe3e0d672d52b458e479f794877c
Amon Schneider <wi19102@dhbw-stuttgart.de>
2020-10-11 16:15:09 +0200

    categoryNames, productNames und categoryNames

--- api/data/categories.js
+++ api/data/categories.js
@@ -1,8 +1,9 @@
 let categories = []
+let categoryNames = ["Otto","Ole","Oskar"]
 for (let i = 0; i < 3; i++) {
   categories.push({
     id: i,
-    name: 'string',
+    name: categoryNames[i],
     imageUrl:
       'https://i.pinimg.com/564x/ad/63/f3/ad63f3314d8a2a129859993b4a30cb80.jpg',
   })
--- api/data/products.js
+++ api/data/products.js
@@ -1,10 +1,13 @@
+let selectableNames = ["Nathalie","Nike","Noah","Neo","Nils"]
+let productNames = ["Anton","Aneliese","Antonia","Alex","Aron","Anton","Anna","Adrian","Amy","Ariel"]
 let products = []
+
 for (let i = 0; i < 10; i++) {
   let selectables = []
   for (let j = 0; j < 5; j++) {
     selectables.push({
       id: j,
-      name: 'string',
+      name: selectableNames[j],
       price: Math.random() * 50 + 10,
       customizable: Math.random() >= 0.5,
       imageUrl: 'https://morris-antikshop.de/media/images/info/IMG_3534_1.jpg',
@@ -12,9 +15,9 @@ for (let i = 0; i < 10; i++) {
   }
   products.push({
     id: i,
-    categoryId: 1,
-    name: 'string',
-    description: 'string',
+    categoryId: Math.floor(Math.random()*3),
+    name: productNames[i],
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
     price: Math.random() * 500 + 50,
     priceStarting: Math.random() >= 0.5,
     customizable: Math.random() >= 0.5,

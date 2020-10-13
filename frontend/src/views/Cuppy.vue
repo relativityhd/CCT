3edd5b1971ab6a8db63483aa8f55eb0295ba0703
Ole Ofterdinger <wi19303@lehre.dhbw-stuttgart.de>
2020-10-13 11:41:31 +0200

    Cuppy with Buttons

--- frontend/src/views/Cuppy.vue
+++ frontend/src/views/Cuppy.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="cuppy" class="visible" @click="uff()">
-  <CuppySpeak id="CuppySpeak" />
+  <div id="cuppy" @click="()=>{cuppyButton()}">
+    <div id="CuppyBubble" @click="(event)=>{event.stopPropagation()}" v-bind:class="showCuppyBubble ? 'visible': 'invisible'">
+      <CuppySpeak />
+    </div>
   </div>
 </template>
 
@@ -10,15 +12,51 @@ export default {
   components: {
     CuppySpeak
   },
-  methods:{
-    uff: () =>{console.log("hi")}
+  methods: {
+    cuppyButton() {
+      this.showCuppyBubble = !this.showCuppyBubble
+     }
+  },
+  data: () => {
+    return {
+      showCuppyBubble: true
+    }
   }
 }
 </script>
 <style lang="sass" scoped>
+
+@keyframes fade-in
+  from
+    opacity: 0
+  to
+    opacity: 1
+@keyframes fade-out
+  from
+    opacity: 1
+  to 
+    opacity: 0
+
 #cuppy
   background-image: url("./../assets/cuppy.svg")
   background-repeat: no-repeat
   background-size: contain
   height: 15rem
+  width: max-content
+  transform: rotate(10deg);
+
+.visible
+  animation-name: fade-in
+  animation-time: 15s
+
+.invisible
+  animation-name: fade-out
+  animation-time: 1s
+  opacity: 0
+
+#CuppyBubble
+  transform: rotate(-10deg);
+  position: relative
+  bottom: 43%
+  left: 15%
 </style>
--- frontend/src/views/CuppySpeak.vue
+++ frontend/src/views/CuppySpeak.vue
@@ -1,33 +1,82 @@
 <template>
-<div id=cuppyspeako>
-  <button>Hilfe</button>
-  <button>NAMENAMENAME</button>
-  <button>Tobo ist doof</button>
-  <button>Philipp ist doof</button>
-  <button>Christopher ist doof</button>
+<div id=speak class=speech-bubble>
+  <div id=buttoncontainer>
+      <router-link to="/" tag="button">{{ $t('location') }}</router-link>
+      <router-link to="/support" tag="button">{{ $t('support') }}</router-link>
+      <router-link to="/about" tag="button">{{ $t('imprint') }}</router-link>
+      <button>Philipp ist doof</button>
+      <button>Christopher ist doof</button>
+  </div>
+
+
 </div>
 </template>
 
 <script>
 
 export default {
-    
+  methods:{
+    log(){
+      console.log("hilfe")
+    }
+  }
 }
 
 </script>
+<i18n>
+{
+  "en":{
+    "location": "I'm from somewhere else!",
+    "support": "I need some help",
+    "imprint": "Who made this page?"
 
+  },
+  "de":{
+    "location": "Ich komme von woanders!",
+    "support": "Ich brauche Hilfe",
+    "imprint": "Wer ist verantwortlich?"
+  },
+  "it":{
+    "location": "Vengo da un altro posto!",
+    "support": "Ho bisogno di aiuto",
+    "imprint": "Chi è il responsabile?"
+  }
+}
+</i18n>
 <style lang="sass" scoped>
-#cuppyspeako
-  background-image: url(./../assets/bubble.svg)
-  background-repeat: no-repeat
-  background-size: contain
+.speech-bubble
+	position: relative
+	background: #ffffff
+	border-radius: .4em
+
+
+.speech-bubble:after 
+	content: ''
+	position: absolute
+	bottom: 0
+	left: 50%
+	width: 0
+	height: 0
+	border: 20px solid transparent
+	border-top-color: #ffffff
+	border-bottom: 0
+	border-left: 0
+	margin-left: -10px
+	margin-bottom: -20px
+
+
+#speak
   display: flex
   flex-direction: column
   height: max-content
-  width: max-content
+  width: min-content
+#buttoncontainer
+  margin: 10px 10px 10px 10px
+  display: flex
+  flex-direction: column
 button
   border: none
   width: max-content
-  
+  margin-top: 3px
 
 </style>

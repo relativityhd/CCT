c5c385fdaa528afd4a296c032d9677c5e1b3a486
Tobias Hölzer <tobiashoelzer@hotmail.com>
2021-01-17 16:21:40 +0100

    IN WOKR

--- backend/src/catalogue.js
+++ backend/src/catalogue.js
@@ -1,24 +1,103 @@
+/**
+ * In the originally Version this backend just requested the data from another API-Gateway.
+ * Since this API-Gateway is down now, I recreated it here with made up data.
+ * Therefore some things like translations are not provided.
+ */
+
 const router = require('express').Router()
-const cfenv = require('cfenv')
-const { get } = require('./axios')
+
+let baseURL
+if (process.env.NODE_ENV === 'production') {
+  baseURL = '//cablecard-cct-backend.eu-de.mybluemix.net'
+} else {
+  baseURL = '//localhost:3000'
+}
 
 router.get('/categories', (req, res) => {
-  get('/catalogue/categories', (e, d) => {
-    if (e) d = {}
-    res.json(d)
-    res.end()
-  })
+  res.json([
+    {
+      "id": 0,
+      "name": "Lowboards",
+      "imageUrl": `${baseURL}/categories/lowboard.jpg`
+    },
+    {
+      "id": 1,
+      "name": "Sideboards",
+      "imageUrl": `${baseURL}/categories/sideboard.jpg`
+    },
+    {
+      "id": 2,
+      "name": "Cupboards",
+      "imageUrl": `${baseURL}/categories/cupboard.jpg`
+    }
+  ])
+  res.end()
 })
 
 router.get('/categories/:id/products', (req, res) => {
-  get(`/catalogue/categories/${req.params.id}/products`, (e, d) => {
-    if (e) d = {}
-    res.json(d)
-    res.end()
-  })
+  if (req.params.id === "0") {
+    res.json([
+      {
+        "id": 100,
+        "name": "Lowboard",
+        "price": 200,
+        "priceStarting": false,
+        "customizable": false,
+        "imageUrl": `${baseURL}/categories/lowboard.jpg`
+      }
+    ])
+  } else if (req.params.id === "1") {
+    res.json([
+      {
+        "id": 110,
+        "name": "Sideboard",
+        "price": 150,
+        "priceStarting": false,
+        "customizable": false,
+        "imageUrl": `${baseURL}/categories/sideboard.jpg`
+      }
+    ])
+  } else {
+    res.json([
+      {
+        "id": 120,
+        "name": "Cupboard",
+        "price": 300,
+        "priceStarting": true,
+        "customizable": true,
+        "imageUrl": `${baseURL}/categories/cupboard.jpg`
+      }
+    ])
+  }
+  res.end()
 })
 
 router.get('/products/:id', (req, res) => {
+  if (req.params.id === "100") {
+    res.json({
+      "id": 1000,
+      "name": "Lowboard",
+      "description": "A simple Lowboard",
+      "price": 200,
+      "priceStarting": false,
+      "customizable": false,
+      "imageUrl": `${baseURL}/categories/lowboard.jpg`
+    })
+  } else if (req.params.id === "110") {
+    res.json({
+      "id": 110,
+      "name": "Sideboard",
+      "description": "A simple Sideboard",
+      "price": 150,
+      "priceStarting": false,
+      "customizable": false,
+      "imageUrl": `${baseURL}/categories/sideboard.jpg`
+    })
+  } else {
+    res.json()
+  }
+  return res.end()
+
   get(`/catalogue/products/${req.params.id}`, (e, product) => {
     if (e) product = {}
     const isCustom = product.customizable

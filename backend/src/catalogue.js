b5921480dde6ea283c938449d3da10fcde4ba9a7
Tobias Hölzer <tobiashoelzer@hotmail.com>
2021-01-17 16:58:03 +0100

    Finish categories

--- backend/src/catalogue.js
+++ backend/src/catalogue.js
@@ -22,8 +22,8 @@ router.get('/categories', (req, res) => {
     },
     {
       "id": 1,
-      "name": "Sideboards",
-      "imageUrl": `${baseURL}/categories/sideboard.jpg`
+      "name": "Highboards",
+      "imageUrl": `${baseURL}/categories/highboard.jpg`
     },
     {
       "id": 2,
@@ -33,88 +33,7 @@ router.get('/categories', (req, res) => {
   ])
   res.end()
 })
-[
-  {
-    "id": 90,
-    "name": "Frame",
-    "price": 120,
-    "selectableCategory": "exterior",
-    "customizable": true,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/korpus-weiss.jpg"
-  },
-  {
-    "id": 91,
-    "name": "Shelf",
-    "price": 20,
-    "selectableCategory": "interior",
-    "customizable": true,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/komplement-boden-weiss__0721164_PE733094_S5.JPG"
-  },
-  {
-    "id": 92,
-    "name": "Clothes rail",
-    "price": 10,
-    "selectableCategory": "interior",
-    "customizable": true,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/komplement-kleiderstange-weiss__0650342_PE706138_S5.JPG"
-  },
-  {
-    "id": 93,
-    "name": "Mesh basket",
-    "price": 20,
-    "selectableCategory": "interior",
-    "customizable": true,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/komplement-netzdrahtkorb-mit-auszugschiene-weiss__0643765_PE702093_S5.JPG"
-  },
-  {
-    "id": 94,
-    "name": "Drawer",
-    "price": 40,
-    "selectableCategory": "interior",
-    "customizable": true,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/komplement-schublade-weiss__0762194_PE751731_S5.JPG"
-  },
-  {
-    "id": 95,
-    "name": "White",
-    "price": 0,
-    "selectableCategory": "material",
-    "customizable": false,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/white.jpg"
-  },
-  {
-    "id": 96,
-    "name": "Maple",
-    "price": 40,
-    "selectableCategory": "material",
-    "customizable": false,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/ahorn.jpg"
-  },
-  {
-    "id": 97,
-    "name": "Walnut",
-    "price": 80,
-    "selectableCategory": "material",
-    "customizable": false,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/nussbaum.jpg"
-  },
-  {
-    "id": 98,
-    "name": "Brich",
-    "price": 40,
-    "selectableCategory": "material",
-    "customizable": false,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/birke.jpg"
-  },
-  {
-    "id": 99,
-    "name": "Lamp",
-    "price": 20,
-    "selectableCategory": "accessories",
-    "customizable": false,
-    "imageUrl": "https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/lamp.jpg"
-  }
-]
+
 router.get('/categories/:id/products', (req, res) => {
   if (req.params.id === "0") {
     res.json([
@@ -131,19 +50,19 @@ router.get('/categories/:id/products', (req, res) => {
     res.json([
       {
         "id": 110,
-        "name": "Sideboard",
+        "name": "Highboard",
         "price": 150,
         "priceStarting": false,
         "customizable": false,
-        "imageUrl": `${baseURL}/categories/sideboard.jpg`
+        "imageUrl": `${baseURL}/categories/highboard.jpg`
       }
     ])
   } else {
     res.json([
       {
-        "id": 120,
+        "id": 2, // is hardcoded in frontend since the bad given API Design
         "name": "Cupboard",
-        "price": 300,
+        "price": 120,
         "priceStarting": true,
         "customizable": true,
         "imageUrl": `${baseURL}/categories/cupboard.jpg`
@@ -156,9 +75,8 @@ router.get('/categories/:id/products', (req, res) => {
 router.get('/products/:id', (req, res) => {
   if (req.params.id === "100") {
     res.json({
-      "id": 1000,
+      "id": 100,
       "name": "Lowboard",
-      "description": "A simple Lowboard",
       "price": 200,
       "priceStarting": false,
       "customizable": false,
@@ -166,56 +84,104 @@ router.get('/products/:id', (req, res) => {
     })
   } else if (req.params.id === "110") {
     res.json({
-      "id": 110,
-      "name": "Sideboard",
-      "description": "A simple Sideboard",
-      "price": 150,
-      "priceStarting": false,
-      "customizable": false,
-      "imageUrl": `${baseURL}/categories/sideboard.jpg`
-    })
+        "id": 110,
+        "name": "Highboard",
+        "price": 150,
+        "priceStarting": false,
+        "customizable": false,
+        "imageUrl": `${baseURL}/categories/highboard.jpg`
+      })
   } else {
-    res.json()
+    res.json({
+      "id": 2, // is hardcoded in frontend since the bad given API Design
+      "name": "Cupboard",
+      "price": 120,
+      "priceStarting": true,
+      "customizable": true,
+      "imageUrl": `${baseURL}/categories/cupboard.jpg`,
+      exteriors: [{
+        "id": 90,
+        "name": "Frame",
+        "price": 120,
+        "selectableCategory": "exterior",
+        "customizable": true,
+        "imageUrl": `${baseURL}/parts/korpus.jpg`
+      }],
+      interiors: [{
+        "id": 91,
+        "name": "Shelf",
+        "price": 20,
+        "selectableCategory": "interior",
+        "customizable": true,
+        "imageUrl": `${baseURL}/parts/boden.jpg`
+      },
+      {
+        "id": 92,
+        "name": "Clothes rail",
+        "price": 10,
+        "selectableCategory": "interior",
+        "customizable": true,
+        "imageUrl": `${baseURL}/parts/kleiderstange.jpg`
+      },
+      {
+        "id": 93,
+        "name": "Mesh basket",
+        "price": 20,
+        "selectableCategory": "interior",
+        "customizable": true,
+        "imageUrl": `${baseURL}/parts/korb.jpg`
+      },
+      {
+        "id": 94,
+        "name": "Drawer",
+        "price": 40,
+        "selectableCategory": "interior",
+        "customizable": true,
+        "imageUrl": `${baseURL}/parts/schublade.jpg`
+      }],
+      materials: [{
+        "id": 95,
+        "name": "White",
+        "price": 0,
+        "selectableCategory": "material",
+        "customizable": false,
+        "imageUrl": `${baseURL}/mats/white.jpg`
+      },
+      {
+        "id": 96,
+        "name": "Maple",
+        "price": 40,
+        "selectableCategory": "material",
+        "customizable": false,
+        "imageUrl": `${baseURL}/mats/ahorn.jpg`
+      },
+      {
+        "id": 97,
+        "name": "Walnut",
+        "price": 80,
+        "selectableCategory": "material",
+        "customizable": false,
+        "imageUrl": `${baseURL}/mats/nussbaum.jpg`
+      },
+      {
+        "id": 98,
+        "name": "Birch",
+        "price": 40,
+        "selectableCategory": "material",
+        "customizable": false,
+        "imageUrl": `${baseURL}/mats/birke.jpg`
+      }],
+      accessories: [{
+        "id": 99,
+        "name": "Lamp",
+        "price": 20,
+        "selectableCategory": "accessories",
+        "customizable": false,
+        "imageUrl": `${baseURL}/parts/lamp.jpg`
+      }]
+    })
   }
-  return res.end()
-
-  get(`/catalogue/products/${req.params.id}`, (e, product) => {
-    if (e) product = {}
-    const isCustom = product.customizable
-    if (e || !isCustom) {
-      res.json(product)
-      return res.end()
-    }
-    get(
-      `/catalogue/products/${req.params.id}/selectables`,
-      (e, selectables) => {
-        product.exteriors = []
-        product.interiors = []
-        product.materials = []
-        product.accessories = []
-        selectables.forEach((selectable) => {
-          switch (selectable.selectableCategory) {
-            case 'exterior':
-              product.exteriors.push(selectable)
-              break
-            case 'interior':
-              product.interiors.push(selectable)
-              break
-            case 'material':
-              product.materials.push(selectable)
-              break
-            case 'accessories':
-              product.accessories.push(selectable)
-              break
-            default:
-              break
-          }
-        })
-        res.json(product)
-        res.end()
-      }
-    )
-  })
+  res.end()
 })
 
 module.exports = router
